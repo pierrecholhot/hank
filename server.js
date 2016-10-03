c2969e2b6a3592cde3fb57da55f19214aaa008c3
Pierre Cholhot <pcholhot@digitas.fr>
2016-10-03 12:32:04 +0200

    Remove some semicolons

--- server.js
+++ server.js
@@ -1,66 +1,66 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const os = require('os');
-const monitor = require('os-monitor');
-const exec = require('child_process').exec;
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const os = require('os')
+const monitor = require('os-monitor')
+const exec = require('child_process').exec
 
 const monitorConfig = {
   delay: monitor.seconds(1),
   immediate: true
-};
+}
 
-const TOTAL_CPU = os.cpus().length;
-const MAX_LOAD = TOTAL_CPU / 4;
+const TOTAL_CPU = os.cpus().length
+const MAX_LOAD = TOTAL_CPU / 4
 
-const history = [];
-const recentEvents = [];
+const history = []
+const recentEvents = []
 
 const alerts = [{
   average: 0,
   highUsage: false,
   timestamp: new Date().getTime()
-}];
+}]
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 app.set('views', './app/views')
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
 app.get('/stress', (req, res) => {
-  stress();
-  res.sendStatus(200);
-});
+  stress()
+  res.sendStatus(200)
+})
 
 app.get('/unstress', (req, res) => {
-  unstress();
-  res.sendStatus(200);
-});
+  unstress()
+  res.sendStatus(200)
+})
 
 app.get('/', (req, res) => {
   res.render('index', {
     totalCPUs: TOTAL_CPU,
     maxLoad: MAX_LOAD
-  });
-});
+  })
+})
 
-monitor.start(monitorConfig);
-monitor.on('monitor', handleMonitorEvent);
+monitor.start(monitorConfig)
+monitor.on('monitor', handleMonitorEvent)
 
-io.on('connection', emitInitialState);
+io.on('connection', emitInitialState)
 
-server.listen(3000, started);
+server.listen(3000, started)
 
 function started() {
-  console.log('Hank is listening on port 3000');
+  console.log('Hank is listening on port 3000')
 }
 
 function stress() {
-  exec('cat /dev/zero > /dev/null');
+  exec('cat /dev/zero > /dev/null')
 }
 
 function unstress() {
-  exec('killall cat');
+  exec('killall cat')
 }
 
 function emitInitialState() {
@@ -68,7 +68,7 @@ function emitInitialState() {
     history,
     cpus: TOTAL_CPU,
     alerts: alerts.filter((a, i) => i !== 0)
-  });
+  })
 }
 
 function handleMonitorEvent(event) {
@@ -76,37 +76,37 @@ function handleMonitorEvent(event) {
     loadavg: event.loadavg[0],
     timestamp: new Date().getTime(),
     usedmemory: Math.floor((event.totalmem - event.freemem) * 100 / event.totalmem)
-  };
+  }
 
-  history.push(data);
-  recentEvents.push(data);
+  history.push(data)
+  recentEvents.push(data)
 
   if (isCollectionFull(recentEvents, monitor.minutes(2))) {
-    const totalLoadAvg = recentEvents.reduce((a, b) => (a.loadavg || a) + b.loadavg);
-    const average = totalLoadAvg / recentEvents.length;
-    checkForAlert(average, data.timestamp);
-    recentEvents.length = 0;
+    const totalLoadAvg = recentEvents.reduce((a, b) => (a.loadavg || a) + b.loadavg)
+    const average = totalLoadAvg / recentEvents.length
+    checkForAlert(average, data.timestamp)
+    recentEvents.length = 0
   }
 
   if (isCollectionFull(history, monitor.minutes(10))) {
-    history.shift();
-    data.historyFull = true;
+    history.shift()
+    data.historyFull = true
   }
 
-  io.emit('monitor', data);
+  io.emit('monitor', data)
 }
 
 function isCollectionFull(col, max){
-  return col[0].timestamp + max <= col[col.length - 1].timestamp;
+  return col[0].timestamp + max <= col[col.length - 1].timestamp
 }
 
 function checkForAlert(average, timestamp) {
-  const highUsage = average > MAX_LOAD;
-  const lastAlert = alerts[alerts.length - 1];
+  const highUsage = average > MAX_LOAD
+  const lastAlert = alerts[alerts.length - 1]
 
   if (lastAlert.highUsage !== highUsage) {
-    const alertInfo = { average, highUsage, timestamp };
-    alerts.push(alertInfo);
-    io.emit('alert', alertInfo);
+    const alertInfo = { average, highUsage, timestamp }
+    alerts.push(alertInfo)
+    io.emit('alert', alertInfo)
   }
 }
